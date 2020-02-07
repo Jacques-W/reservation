4ed692cda1f28d20ef460ff3bf04754b2f92c7c0
Klarth67 <f.allouache@hotmail.fr>
2020-02-07 12:22:54 +0100

    Clientcontroller

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -2,17 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\UserRepository;
+
 
 class ClientController extends AbstractController
 {
     /**
-     * @Route("/client", name="client")
+     * @Route("/register", name="register")
      */
-    public function index()
+    public function user(Request $request)
     {
-        return $this->render('client/index.html.twig', [
+
+        $em = $this->getDoctrine()->getManager();
+        $request->isMethod('POST');
+        $data = $request->request->all();
+
+        $client = (new User())
+            ->setEmail($data['email'])
+            ->setRoles($data['roles'])
+            ->setPassword($data['password'])
+            ->setRoles($data['nom'])
+            ->setEmail($data['prenom'])
+            ->setRoles($data['adresse'])
+            ->setEmail($data['codepostal'])
+            ->setRoles($data['ville'])
+            ->setRoles($data['telephone']);
+
+            $em->persist($client);
+            $em->flush();
+
+        return $this->render('client/register.html.twig', [
             'controller_name' => 'ClientController',
         ]);
     }
---
+++ src/Entity/Event.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EventRepository")
+ */
+class Event
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $artiste;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $presentation;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $heure;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adresse;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $tarif;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getArtiste(): ?string
+    {
+        return $this->artiste;
+    }
+
+    public function setArtiste(string $artiste): self
+    {
+        $this->artiste = $artiste;
+
+        return $this;
+    }
+
+    public function getPresentation(): ?string
+    {
+        return $this->presentation;
+    }
+
+    public function setPresentation(string $presentation): self
+    {
+        $this->presentation = $presentation;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getHeure(): ?string
+    {
+        return $this->heure;
+    }
+
+    public function setHeure(string $heure): self
+    {
+        $this->heure = $heure;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getTarif(): ?int
+    {
+        return $this->tarif;
+    }
+
+    public function setTarif(int $tarif): self
+    {
+        $this->tarif = $tarif;
+
+        return $this;
+    }
+}
