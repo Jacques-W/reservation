5bab018918fc401fbbb10337011efe37bb783f04
jacques-w <jacques.developpement.web@gmail.com>
2020-02-07 09:31:13 +0100

    Base Propre

---
+++ src/Form/RegisterClientType.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
+class RegisterClientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse Mail',
+                'attr'=> [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('roles')
+            ->add('password', TextType::class, [
+                'label' => 'Mot de Passe',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('nom', TextType::class, [
+                'label' => 'Votre Nom',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Votre Prénom',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('adresse', TextType::class, [
+                'label' => 'Votre Adresse',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('codepostal', TextType::class, [
+                'label' => 'Votre Code Postal',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('ville', TextType::class, [
+                'label' => 'Votre Ville',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('telephone', TextType::class, [
+                'label' => 'Votre Numéro de Téléphone',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('token')
+            ->add('active')
+            ->add('created_at')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
