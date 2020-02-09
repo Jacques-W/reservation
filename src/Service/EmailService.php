57d9d23d30972b4e762a508ff698e2c9d67086cc
Jacques-W <jacques.developpement.web@gmail.com>
2020-02-09 20:30:53 +0100

    Ajout du form  (src\form) EventModifType ajout des migrations et service(service partiel)
    Mise à jour du repo

---
+++ src/Service/EmailService.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+
+class EmailService
+{
+
+	private $mailer;
+
+	public function __construct(
+		MailerInterface $mailer
+	) {
+		$this->mailer = $mailer;
+		$this->mon_email = 'demo.wf3.victor@gmail.com';
+		$this->data = array(
+			'from' => false,
+			'to' => false,
+			'replyTo' => false,
+			'subjet' => false,
+			'template' => false,
+			'contexte' => false
+
+		);
+	}
+
+	
+
+	public function send($data)
+	{
+
+		// Envoyer l'email
+		$data = array_merge($this->data, $data);
+
+		if (!$data['from']) {
+			$data['from'] = $_ENV['MY_EMAIL'];
+		}
+		if (!$data['to']) {
+			$data['to'] = $_ENV['MY_EMAIL'];
+		}
+		if (!$data['replyTo']) {
+			$data['replyTo'] = $data['from'];
+		}
+
+		if ($_ENV['APP_ENV'] == 'dev') {
+			$data['to'] = $_ENV['MY_EMAIL'];
+		}
+		$email = (new TemplatedEmail())
+			->from($data['from'])
+			->to($data['to'])
+			->replyTo($data['replyTo'])
+			->subject($data['subject'])
+			->htmlTemplate($data['template'])
+			->context($data['context']);
+		$this->mailer->send($email);
+	}
+
+	public function register($user)
+	{
+		$data = array(
+			'to' => $user->getEmail(),
+			'subject' => "Confirmez votre inscription",
+			'template' => 'emails/security/register.email.twig',
+			'context' => ['user'=>$user]
+
+		);
+		$this->send($data);
+	}
+
+
+
+
+}
